57cceaa168ec35a3e86343f4f8d12b260e42fa52
Bhavya <shahbhavya5800@gmail.com>
2023-01-08 12:26:05 +0530

    change admin deskboard

--- admin/counters/guest-count.php
+++ admin/counters/guest-count.php
@@ -1,7 +1,7 @@
 <?php
     include '../includes/dbconn.php';
 
-    $sql = "SELECT id FROM abab_booking";
+    $sql = "SELECT id FROM guest_details";
                 $query = $mysqli->query($sql);
                 echo "$query->num_rows";
 ?>
--- admin/counters/vyavastha-count.php
+++ admin/counters/vyavastha-count.php
@@ -1,7 +1,7 @@
 <?php
     include '../includes/dbconn.php';
 
-    $sql = "SELECT id FROM abab_booking";
+    $sql = "SELECT id FROM abab_vyavastha";
                 $query = $mysqli->query($sql);
                 echo "$query->num_rows";
 ?>
